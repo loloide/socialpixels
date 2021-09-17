3db8df3f475088f7ca58d000c4bba0d41548b21c
loloide <lorendlc2005@gmail.com>
2021-09-17 18:08:38 -0300

    comment users class

--- src/services/pixels/pixels.class.js
+++ src/services/pixels/pixels.class.js
@@ -1,5 +1,16 @@
 const { Service } = require('feathers-memory');
 
 exports.Pixels = class Pixels extends Service {
-  
+    // create (data, params) {
+    //     // This is the information we want from the user signup data
+    //     const { email, password, githubId, name } = data;
+    //     // Use the existing avatar image or return the Gravatar for the email
+    //     const avatar = data.avatar || getGravatar(email);
+    //     // The complete user
+    //     const userData = {
+    //       email,
+    //       name,
+    //       password,
+    //       githubId,
+    //       avatar};
 };
