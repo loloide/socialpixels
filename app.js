a10215a83dd8a0d0c16b55b6bb098f24148d3d2c
loloide <lorendlc2005@gmail.com>
2021-09-20 18:43:18 -0300

    still not working

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ var app = express();
 
 // Set up the server
 // process.env.PORT is related to deploying on heroku
-var server = app.listen(process.env.PORT || 8080, listen);
+var server = app.listen(process.env.PORT || 8090, listen);
 
 // This call back just tells us that the server has started
 function listen() {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Example app listening at http://localhost:8080');
+  console.log('Example app listening at http://localhost:8090');
 }
 
 app.use(express.static('public'));
